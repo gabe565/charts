f5fcaa57e1d902634aba077daebd4c33ee20b4ed
Gabe Cook <gabe565@gmail.com>
2023-01-30 22:27:41 -0600

    :recycle: (ci): Move doc perparation to hack script

---
+++ hack/prepare-docs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+export IFS=$'\n\t'
+
+repo_path="${1:-https://github.com/gabe565/charts}"
+
+cd "$(git rev-parse --show-toplevel)"
+
+cp README.md docs/README.md
+cp .gitignore docs/.gitignore
+
+CHARTS=()
+
+# Copy readmes
+while IFS= read -r -d '' f; do
+  mkdir -p "docs/$(dirname "$f")"
+
+  sed '/^\*\*Homepage:\*\*/,+1d' "$f" \
+    | sed "s|(\./\(.*\.yaml\))|($repo_path/blob/main/$(dirname "$f")/\1)|g" \
+    > "docs/$f"
+
+  CHARTS+=("$(basename "$(dirname "$f")")")
+done < <(find charts -type f -name '*.md' -not -path './docs/*' -print0)
+
+# Add charts to sidebar
+CHARTS="$(printf '%s\n' "${CHARTS[@]}" | sort -h)" yq -i \
+  '(.docs[] | select(.title == "Charts") | .children) = [(strenv(CHARTS) | split("\n") | .[] | {"title": ., "url": "/charts/" + . + "/"})]' \
+  docs/_data/navigation.yml
