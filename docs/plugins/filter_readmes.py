2fcfc590d9cc40b71a7824f321ebcb7e28b688cd
Gabe Cook <gabe565@gmail.com>
2023-03-23 19:00:44 -0500

    :bulb: (docs): Add code comments to chart readme filter

--- docs/plugins/filter_readmes.py
+++ docs/plugins/filter_readmes.py
@@ -34,12 +34,15 @@ def filter_repo_readme(markdown: str, page: Page, config: Config):
 
 
 def filter_chart_readme(markdown: str, page: Page, config: Config):
+    # Remove homepage section
     homepage_re = re.compile(r"^\*\*Homepage:\*\*.*$", re.MULTILINE)
     markdown = homepage_re.sub("", markdown)
 
+    # Change YAML link targets to point to the repo
     yaml_re = re.compile(r"\(\./(.*\.yaml)\)")
     markdown = yaml_re.sub(f"({config.repo_url}/blob/main/{page.url}\\1)", markdown)
 
+    # Set metadata from Chart.yaml
     yaml_file = os.path.join(os.path.dirname(page.file.abs_src_path), "Chart.yaml")
     with open(yaml_file) as f:
         contents = yaml.load(f)
